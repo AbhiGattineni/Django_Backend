3cf0ca5daee62e422fcd8c9f5fe3d5ed9038165e
gundlatrinath <trinatgundla@gmail.com>
2024-10-08 20:59:39 +0530

    updated settings to handle files

--- Django_Backend/settings.py
+++ Django_Backend/settings.py
@@ -75,6 +75,7 @@ CORS_ORIGIN_WHITELIST = (
     "http://localhost:8000",
     "http://localhost:3000",
     "https://www.anddhengroup.com",
+    "http://127.0.0.1:8000",
 )
 
 ROOT_URLCONF = 'Django_Backend.urls'
@@ -156,6 +157,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
